84a5725ad37eb90654b2deca149d6a0db9850eb9
Mr-8lobby <Joshuaaspencer@gmail.com>
2018-06-06 01:31:14 +0000

    Update port_chopper.sh

--- port_chopper.sh
+++ port_chopper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#This should be used after TCP/UDP normal port scan output has be genrerated and saved
+#This should be used after TCP/UDP normal port scan output has be generated (-oN option in nmap) and saved to a file
 #This takes in that output file, parses the open ports, and returns them in a line for version guessing nmap scan
 if [[ "$#" -ne 1 ]]; then
 	echo "Usage: $0 [raw nmap output]"
