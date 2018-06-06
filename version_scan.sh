59f343a35dac5ca67291b977ea32964b53be9084
Mr-8lobby <Joshuaaspencer@gmail.com>
2018-06-06 01:27:49 +0000

    Update version_scan.sh

--- version_scan.sh
+++ version_scan.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
+#takes in an ip address, and the ouput from port_chopper.sh and does a version scan on the ports.
 if [[ "$#" -ne 2 ]] ; then
 	echo "Usage: $0 [ip address] [comma separated ports]"
 else
